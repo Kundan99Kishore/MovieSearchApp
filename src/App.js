090e96365ec142fb7b9be3fa75ed324a1fe641e7
kundan <kundan.kishore.95@gmail.com>
2022-04-26 23:32:32 +0530

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [movie, setMovie] = useState([]);
+  const [input, setInput] = useState("");
+
+  const fetchMovie = (e) => {
+    e.preventDefault();
+    console.log(input);
+    axios.get(`https://www.omdbapi.com/?s=${input}&apikey=43033444`)
+      .then((response) => {
+        console.log(response);
+        setMovie(response.data.Search)
+        console.log(movie);
+      })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form className="form" onSubmit={fetchMovie}>
+        <input id="searchbar" value={input} onInput={e => setInput(e.target.value)} type="text" placeholder="Search Movie.."></input>
+        <button id="btn" onClick={fetchMovie}>Search</button>
+      </form>
+
+
+      <div className='row'>
+        {
+
+          movie.map((value, index) => {
+
+            return (
+              <>
+                <div class="column" key={index}>
+                  <img src={value.Poster} alt={value.Title} style={{ width: "100%" }} />
+                  {value.Title} {value.Year}
+                </div>                                    
+              </>
+            );
+          })  
+
+        }
+      </div>
+    </>
+  )
 }
 
 export default App;
